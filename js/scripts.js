9652cf28af5eb9cc65412be7939921ce3a675dd3
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-07 10:25:43 -0700

    add co-authors
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>
    co-authored-by: Ben Goodrow <bpgoodrow@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business Logic
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function (element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}

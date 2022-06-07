5fb742200d116cf199eb6decd63ad7f67a01f03e
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-07 13:01:22 -0700

    add several tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 // Business Logic
 
-function wordCounter(text) {
+// function wordCounter(text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function (element) {
+//     if (!Number(element)) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+function numberOfOccurrencesInText(word, text) {
   if (text.trim().length === 0) {
     return 0;
   }
-  let wordCount = 0;
   const wordArray = text.split(" ");
+  let wordCount = 0;
   wordArray.forEach(function (element) {
-    if (!Number(element)) {
+    if (word.toLowerCase() === element.toLowerCase()) {
       wordCount++;
     }
   });
